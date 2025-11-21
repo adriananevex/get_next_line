eb7f7b69aeeaf69d4a0c8bda1d8be5329fa402ea
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2025-11-21 16:00:24 +0000

    begin

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 14
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+size_t  ft_strlen(const char *str);
+
+char    *get_next_line(int fd);
+char    *ft_strdup(const char *str);
+char    *ft_strchr(char *str, int c);
+char    *ft_strjoin(char *s1, const char *s2);
+char    *ft_ft_substr(const char *str, size_t start, size_t len);
+
+#endif
