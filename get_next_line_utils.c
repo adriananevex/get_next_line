af41e23ca18de1653aa42b296fc11a4f8ed6951f
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2025-11-21 16:56:46 +0000

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,130 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	unsigned char	d;
+	size_t			i;
+
+	i = 0;
+	d = (unsigned char)c;
+	while (1)
+	{
+		if ((unsigned char)s[i] == d)
+		{
+			return ((char *)&s[i]);
+		}
+		if (s[i] == '\0')
+		{
+			return (NULL);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	unsigned char	d;
+	size_t			i;
+
+	i = 0;
+	d = (unsigned char)c;
+	while (1)
+	{
+		if ((unsigned char)s[i] == d)
+		{
+			return ((char *)&s[i]);
+		}
+		if (s[i] == '\0')
+		{
+			return (NULL);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(const char *str)
+{
+	int		i;
+	char	*copy;
+
+	i = 0;
+	copy = ft_calloc(ft_strlen(str) + 1, sizeof(char));
+	if (!copy)
+		return (NULL);
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	*ft_substr(const char *str, size_t start, size_t len)
+{
+	size_t	atual_len;
+	char	*sub;
+	size_t	i;
+
+	if (!c)
+		return (NULL);
+	if (start >= ft_strlen(str))
+		return (ft_strdup(""));
+	if (len > ft_strlen(str + start))
+		atual_len = ft_strlen(c + start);
+	else
+		atual_len = len;
+	sub = (char *)ft_calloc(atual_len + 1, sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < atual_len)
+	{
+		sub[i] = str[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char	*ft_strjoin(char *s1, const char *s2)
+{
+	int		i;
+	int		j;
+	char	*copy;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	copy = malloc(ft_strlen(s1) + ft_strlen(s2) + 1 * sizeof(char));
+	if (!copy)
+		return (free(s1), NULL);
+	while (s1[i])
+	{
+		copy[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		copy[i + j] = s2[j];
+		j++;
+	}
+	copy[i + j] = '\0';
+    free(s1);
+	return (copy);
+}
