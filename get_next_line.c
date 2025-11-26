6bea4790a98f75ec9040990cf0bca1e06589dfdb
Adriana Neves <158789061+adriananevex@users.noreply.github.com>
2025-11-26 17:29:25 +0000

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -67,7 +67,7 @@ char	*get_next_line(int fd)
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
 		free(left);
 		free(buffer);
